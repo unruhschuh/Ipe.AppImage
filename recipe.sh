4cdafc5e2e7c752cfe74219b7c32e0905f264fca
Thomas Leitz <thomas.leitz@web.de>
2016-01-30 07:25:51 +0100

    edited recipe.sh

--- recipe.sh
+++ recipe.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Halt on errors
+set -e
+
 #wget http://download.qt.io/official_releases/qt/5.5/5.5.1/qt-opensource-linux-x64-5.5.1.run
 
 # Change directory to build. Everything happens in build.
