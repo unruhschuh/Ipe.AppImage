7559a840651881b45fdb2cd1afec6808dfa26932
Thomas Leitz <thomas.leitz@web.de>
2016-01-30 05:53:51 +0100

    edited recipe.sh

--- recipe.sh
+++ recipe.sh
@@ -2,9 +2,6 @@
 
 #wget http://download.qt.io/official_releases/qt/5.5/5.5.1/qt-opensource-linux-x64-5.5.1.run
 
-# become root
-sudo su
-
 # Change directory to build. Everything happens in build.
 mkdir build
 cd build
@@ -13,14 +10,29 @@ cd build
 # download packages
 ######################################################
 # epel-release for newest Qt
-yum -y install epel-release git
+sudo yum -y install epel-release git
 
 # Need a newer gcc, getting it from Developer Toolset 2
-wget http://people.centos.org/tru/devtools-2/devtools-2.repo -O /etc/yum.repos.d/devtools-2.repo
-yum -y install devtoolset-2-gcc devtoolset-2-gcc-c++ devtoolset-2-binutils
+sudo wget http://people.centos.org/tru/devtools-2/devtools-2.repo -O /etc/yum.repos.d/devtools-2.repo
+sudo yum -y install devtoolset-2-gcc devtoolset-2-gcc-c++ devtoolset-2-binutils
 # /opt/rh/devtoolset-2/root/usr/bin/gcc
 # now holds gcc and c++ 4.8.2
-export PATH=$PATH:/opt/rh/devtoolset-2/root/usr/bin/
+scl enable devtoolset-2 bash
+
+######################################################
+# build libraries from source
+######################################################
+# libjpeg
+wget http://www.ijg.org/files/jpegsrc.v8d.tar.gz
+tar xfvz jpegsrc.v8d.tar.gz
+cd jpeg-8d
+./configure && make && sudo make install
+cd ..
+
+# lua
+wget http://www.lua.org/ftp/lua-5.3.2.tar.gz
+tar xfvz lua-5.3.2.tar.gz
+
 
 
 wget https://dl.bintray.com/otfried/generic/ipe/7.2/ipe-7.2.2-src.tar.gz
